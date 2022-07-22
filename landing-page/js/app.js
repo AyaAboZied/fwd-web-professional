ccd8693ceb2bcbfcc46f6329a718c741f516c9ab
AyaAboZied <68289149+AyaAboZied@users.noreply.github.com>
2022-07-22 21:34:24 +0200

    Update app.js

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -22,7 +22,7 @@
  * Define Global Variables
  * 
  */
-const sec = document.querySelectorAll('section');
+let sec = document.querySelectorAll('section');
 const menu = document.getElementById('navbar__list');
 const fragment = document.createDocumentFragment();
 
@@ -34,12 +34,12 @@ const fragment = document.createDocumentFragment();
 //to Creat the navigation bar
 function creatNav() {
     for (let s of sec) {
-        let secName = s.getAttribute('data-nav');
-        let secLink = s.getAttribute('id');
+        let sName = s.getAttribute('data-nav');
+        let sLink = s.getAttribute('id');
         //create an item for each one
         let List = document.createElement('li');
         //add the text inside the li element 
-        List.innerHTML = `<a class="menu__link" href="#${secLink}">${secName}</a>`;
+        List.innerHTML = `<a class="menu__link" href="#${sLink}">${sName}</a>`;
         //add the item text 
         fragment.appendChild(List);
     }
@@ -48,9 +48,9 @@ function creatNav() {
 }
 
 //Determines if section is in viewport
-function secInView(e) {
-    let secPos = e.getBoundingClientRect();
-    return (secPos.top >= 0);
+function secInView(elem) {
+    let sPos = elem.getBoundingClientRect();
+    return (sPos.top >= 0);
 }
 
 //Gives the section viewed a different appearance
@@ -58,47 +58,43 @@ function toggleActiveState() {
     for (let s of sec) {
         if (secInView(s)) {
             //use toggle to add or remove this class 
-            if (!s.classList.contains('your-active-class'))
-                s.classList.add('your-active-class');
-        } else {
-            s.classList.remove('your-active-class');
+            s.classList.toggle('your-active-class');
         }
     }
-}
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
- */
+    /**
+     * End Helper Functions
+     * Begin Main Functions
+     * 
+     */
 
-// build the nav
+    // build the nav
 
-creatNav();
-// Add class 'active' to section when near top of viewport
-window.addEventListener('scroll', toggleActiveState);
-// Scroll to top of page
-let span = document.querySelector('.up');
-window.onscroll=function(){
-    this.scrollY>=800?span.classList.add('show'):span.classList.remove('show');
-}
-span.onclick=function(){
-    window.scrollTo({
-        top:0,
-        behavior:"smooth"
-    });
-        
-};
+    creatNav();
+    // Add class 'active' to section when near top of viewport
+    window.addEventListener('scroll', toggleActiveState);
+    // Scroll to top of page
+    let span = document.querySelector('.up');
+    window.onscroll = function () {
+        this.scrollY >= 800 ? span.classList.add('show') : span.classList.remove('show');
+    }
+    span.onclick = function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
 
-/**
- * End Main Functions
- * Begin Events
- * 
- */
-// Hide fixed navigation bar while not scrolling
+    };
 
-// Build menu 
+    /**
+     * End Main Functions
+     * Begin Events
+     * 
+     */
+    // Hide fixed navigation bar while not scrolling
 
-// Scroll to section on link click
+    // Build menu 
 
-// Set sections as active
+    // Scroll to section on link click
+
+    // Set sections as active
